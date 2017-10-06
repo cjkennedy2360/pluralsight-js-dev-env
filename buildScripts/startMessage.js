7f483e6f8f5bc3dffdc9fbac5d1a722ff41483a8
CJ Kennedy <cjkennedy2360@gmail.com>
2017-10-06 01:06:15 -0600

    Module 9 Work in Progress

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,9 @@
+/* startMessage.js */
+/*eslint-env node*/
+/*jslint node:true*/
+
+"use Strict";
+
+import chalk from "chalk";
+
+console.log(chalk.green("Starting App in Dev Mode...")); // eslint-disable-line no-console
